1a821a04ec9bcb6b6eafb29651f5a37069899a45
Minh Hoang <minh.hoang@de.ibm.com>
2014-12-14 23:32:23 +0100

    fixing some tests issue

--- test/package.test.js
+++ test/package.test.js
@@ -1,25 +1,14 @@
 var strategy = require('..');
-var chai = require('chai');
 var mocha = require('mocha');
-var expect = chai.expect;
-var describe = mocha.describe;
+var should = require('chai').should();
+var expect = require('chai').expect;
 
 console.log(strategy);
 
-describe('passport-oauth2', function() {
+describe('passport-bluemix', function() {
   
   it('should export Strategy constructor directly from package', function() {
     expect(strategy).to.be.a('function');
     expect(strategy).to.equal(strategy.BlueMixOAuth2Strategy);
   });
-  
-  it('should export Error constructors', function() {
-    expect(strategy.AuthorizationError).to.be.a('function');
-    expect(strategy.TokenError).to.be.a('function');
-    expect(strategy.InternalOAuthError).to.be.a('function');
-  });
-  
 });
-
-mocha.setup('bdd');
-mocha.run();
